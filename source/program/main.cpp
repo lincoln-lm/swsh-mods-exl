6c1d41cc15abdd7c4e37a90b9d86199036cb8b09
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2025-05-26 19:54:16 -0600

    evolution randomization

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -12,6 +12,7 @@
 #include "patches/route_restriction.hpp"
 #include "patches/field_items.hpp"
 #include "patches/wild_pokemon.hpp"
+#include "patches/evolution.hpp"
 #ifdef DEBUG
 #include "patches/debug.hpp"
 #endif
@@ -34,6 +35,7 @@ extern "C" void exl_main(void* x0, void* x1) {
     install_route_restriction_patch();
     install_field_items_patch();
     install_wild_pokemon_patch();
+    install_evolution_patch();
     install_hid_patch();
 #ifdef DEBUG
     install_debug_patch();
---
+++ source/program/patches/evolution.hpp
@@ -0,0 +1,35 @@
+struct evolution_method_t {
+    u16 method;
+    u16 argument;
+    u16 species;
+    s8 form;
+    u8 level;
+} PACKED;
+
+struct evolution_set_t {
+    u64 unk;
+    u32 species;
+    u16 form;
+    u16 padding;
+    evolution_method_t (*methods)[9];
+} PACKED;
+
+HOOK_DEFINE_TRAMPOLINE(CustomEvolutionBinary) {
+    static void Callback(evolution_set_t* out, u32 species, u32 form) {
+        Orig(out, species, form);
+        const std::string seed = std::format("evolution_binary_{}_{}", species, form);
+        auto rng = RngManager::NewRandomGenerator(std::span(seed));
+        for (int i = 0; i < 9; i++) {
+            evolution_method_t& method = (*out->methods)[i];
+            // maintain vanilla method but swap species
+            auto [species, form] = rng.RandSpeciesAndForm();
+            method.species = species;
+            method.form = form;
+        }
+    }
+};
+
+void install_evolution_patch() {
+    // TODO: symbol LoadEvolutionBinary
+    CustomEvolutionBinary::InstallAtOffset(0x786150 - VER_OFF);
+}
