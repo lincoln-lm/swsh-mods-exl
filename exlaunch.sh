61dc6d8df4347c0fd3a30373a117502e9dfdce80
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2025-08-01 15:23:23 -0600

    eden deploy script

--- exlaunch.sh
+++ exlaunch.sh
@@ -18,10 +18,12 @@ elif [ "$1" == "deploy-ftp" ]; then
     MAKE_VERB="deploy-ftp"
 elif [ "$1" == "deploy-ryu" ]; then
     MAKE_VERB="deploy-ryu"
+elif [ "$1" == "deploy-eden" ]; then
+    MAKE_VERB="deploy-eden"
 elif [ "$1" == "make-npdm-json" ]; then
     MAKE_VERB="npdm-json"
 else
-    echo "Invalid arg. (build/clean/deploy-sd/deploy-artifact/deploy-ftp/deploy-ryu)"
+    echo "Invalid arg. (build/clean/deploy-sd/deploy-artifact/deploy-ftp/deploy-ryu/deploy-eden)"
     exit 1
 fi
 
