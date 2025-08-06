615f8c98eb5fd41b60d9ae01b6ba462733d66574
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2025-08-05 20:04:59 -0600

    fix eden script for ironmon

--- misc/scripts/deploy-eden.sh
+++ misc/scripts/deploy-eden.sh
@@ -8,12 +8,11 @@ if [ -z "${EDEN_PATH}" ]; then
 fi
 
 # Setup the path to the game's mods folder.
-export MODS_PATH=${EDEN_PATH}/load/${PROGRAM_ID}/swsh-mods-exl
+export MODS_PATH=${EDEN_PATH}/load/${PROGRAM_ID}/ironmon
 
 # Ensure directory exists.
 mkdir -p ${MODS_PATH}/
-mkdir -p ${EDEN_PATH}/sdmc/config/swsh-mods-exl
+mkdir -p ${EDEN_PATH}/sdmc/config/ironmon
 
 # Copy over files.
 cp -r ${OUT}/* ${MODS_PATH}/
-cp config.toml ${EDEN_PATH}/sdmc/config/swsh-mods-exl/
